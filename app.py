3cf9896ff14f5f0be83a12b32105a6162186e200
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-02-07 23:30:03 -0300

    add routes

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+@app.route('/register')
+def register():
+   return render_template('register.html')
+
+@app.route('/members')
+def members():
+   return render_template('members.html')
 
 if __name__ == '__main__':
    app.run()
